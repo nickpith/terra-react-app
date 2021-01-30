12bf58cc9e791a653a168c866e8098ecb0dbd56b
Nick Smith <nickpith@gmail.com>
2021-01-30 12:48:14 -0600

    Setup Terra UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
+import Button from 'terra-button';
+import Base from 'terra-base';
+import { injectIntl } from 'react-intl';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// This value could be sent from the server as well
+const locale = (navigator.languages && navigator.languages[0])
+               || navigator.language
+               || navigator.userLanguage
+               || 'en';
+
+const AppHeader = injectIntl(({ intl }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <Button 
+        text={intl.formatMessage({id: 'App.Button'})}
+        variant="de-emphasis"
+      />
+    </header>
+  );
+});
+
+function App(props) {
+  return (
+    <Base locale={locale}>
+      <AppHeader />
+    </Base>
   );
 }
 
